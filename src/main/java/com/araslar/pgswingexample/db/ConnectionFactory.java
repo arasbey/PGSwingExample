b031aa24923642248facfb628ee0571ee0f16816
Burak ARAS <31282950+arasbey@users.noreply.github.com>
2017-10-08 22:52:29 +0300

    initialize
    initialize,db,java,jdbc

---
+++ src/main/java/com/araslar/pgswingexample/db/ConnectionFactory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.araslar.pgswingexample.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author PC
+ */
+public class ConnectionFactory {
+    
+    private static final String url = "jdbc:postgresql://127.0.0.1:5432/javadb";
+    private static final String username = "postgres";
+    private static final String password = "postgres";
+    
+  public static Connection  getConnection(){
+   Connection connection = null;
+   
+      try {
+          Class.forName("org.postgresql.Driver");
+          connection = DriverManager.getConnection(url, username, password);
+      } catch (Exception e) {
+          e.setStackTrace(null);
+      }
+  return connection;
+    
+    
+  }  
+}
